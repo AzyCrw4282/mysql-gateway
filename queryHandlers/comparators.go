9c12c30d0c1907f631d7cfa8e2f49440b368fd80
zeac084 <zeac084@live.rhul.ac.uk>
2020-08-29 01:38:11 +0000

    notes to help on comparators.go

--- queryHandlers/comparators.go
+++ queryHandlers/comparators.go
@@ -1,7 +1,11 @@
 package queryHandlers
 
+// input string x=eq.y
 type comparators struct {
-	Field         string
-	ComparatorObj string
-	Value         string
+	Field         string //x
+	ComparatorObj string //eq
+	Value         string //y
 }
+
+//possible methods using receiver objects
+///get for all fields
